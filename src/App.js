f8c79a480bed1c86d723b655f180034fd5f658ce
Govind Brahmanyapura <brahmany@grinnell.edu>
2021-03-20 17:40:49 -0500

    redirect http to https

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Home from "./Home";
 import Unsubscribe from "./Unsubscribe";
-import Update from './Update'
+import Update from "./Update";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import HttpsRedirect from "react-https-redirect";
 
 function App() {
   return (
     <>
-      <Router>
-        <Route exact path="/" component={Home}/>
-        <Route path="/unsubscribe/:emailToken" component={Unsubscribe} />
-        <Route path="/update/:emailToken" component={Update} />
-      </Router>
+      <HttpsRedirect>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/unsubscribe/:emailToken" component={Unsubscribe} />
+          <Route path="/update/:emailToken" component={Update} />
+        </Router>
+      </HttpsRedirect>
     </>
   );
 }
